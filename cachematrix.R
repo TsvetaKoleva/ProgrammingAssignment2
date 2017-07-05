981aab4e7bc8c88cf5d3f42f22bbe191ffaa171e
TsvetaKoleva <tsveta.koleva@outlook.com>
2017-07-05 13:46:51 +0300

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -5,36 +5,38 @@
 
 ## Below you can find a function that creates a special matrix and caches its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
-        inv <- NULL
-        set <- function(y) {
+makeCacheMatrix <- function(x = matrix()) { # initializing an empty default matrix
+        inv <- NULL   # and an object in the environment to be used later
+        set <- function(y) { #defining the method of assignment in parent environment
             x <<- y
             inv <<- NULL
-        }
-        get <- function() x
-        setinv <- function(solve) inv <<- solve
+        } 
+        
+        get <- function() x #through lexical scoping R retrieves currently undefined 
+                            #element stored in parent environment
+        setinv <- function(solve) inv <<- solve  #setting and getting the inverse
         getinv <- function() inv
-        list(set = set, get = get,
-             setinv = setinv,
+        list(set = set, get = get,  #combining the functions within a list to be 
+             setinv = setinv,       #returned in the parent environment
              getinv = getinv)
     
 }
 
 
 
-## The below function (cacheSolve) takes the special matrix form the function above 
+## The below function (cacheSolve) takes the special matrix from the function above 
 ## (makeCacheMatrix) and  calculates its inverse. Has the matrix not changed, 
 ## in the event that the inverse has been calculated and cached, the result is  
 ## retrieved from the cache instead of being calculated again.
 
-cacheSolve <- function(x, ...) {
-    inv <- x$getinv()
-    if(!is.null(inv)) {
-        message("getting cached data")
+cacheSolve <- function(x, ...) {    #defining a function with arguments x and additional ones 
+    inv <- x$getinv()               #attempting to retrieve the inverse by:
+    if(!is.null(inv)) {             #checking whether or not it has already been calculated and stored
+        message("getting cached data")  #in the parent environment and retrieving it from the cache if so.
         return(inv)
     }
-    data <- x$get()
-    inv <- solve(data, ...)
+    data <- x$get()                 #if not, calling previously defined setinv function 
+    inv <- solve(data, ...)         #on input argument and returning the inverse of the matrix.   
     x$setinv(inv)
     return(inv)
 }
